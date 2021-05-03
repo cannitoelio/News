84bc758ff32445da062b21fe25048a1a9ac7b12c
Elio Cannito <cannito.elio@gmail.com>
2021-05-03 22:33:23 +0200

    Aggiunto il find by id :)

--- src/main/java/com/coding/challenge/news/services/NotiziaService.java
+++ src/main/java/com/coding/challenge/news/services/NotiziaService.java
@@ -5,6 +5,7 @@ import com.coding.challenge.news.models.entities.Notizia;
 import com.coding.challenge.news.models.forms.NotiziaForm;
 import com.coding.challenge.news.repositories.NotiziaRepository;
 import com.coding.challenge.news.util.ObjectMapperUtils;
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import org.springframework.ui.ModelMap;
 import java.util.List;
 
 @Service
+@Log4j2
 public class NotiziaService {
     @Autowired
     NotiziaRepository notiziaRepository;
@@ -22,7 +24,8 @@ public class NotiziaService {
     }
 
     public NotiziaDto getNotizia(long id) {
-        return ObjectMapperUtils.map(notiziaRepository.findById(id),NotiziaDto.class);
+        Notizia notizia = notiziaRepository.findById(id).orElse(new Notizia());
+        return ObjectMapperUtils.map(notiziaRepository.findById(id).orElse(new Notizia()),NotiziaDto.class);
     }
 
     public void deleteNotizia(long id) {
