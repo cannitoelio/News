1d75279451e9155e3b40d522398e70efdfa5d852
Elio Cannito <cannito.elio@gmail.com>
2022-04-29 22:43:38 +0200

    Clean imports

--- src/main/java/com/coding/challenge/news/costants/Errors.java
+++ src/main/java/com/coding/challenge/news/costants/Errors.java
@@ -1,11 +1,7 @@
 package com.coding.challenge.news.costants;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-
 public class Errors {
 
     public static final String ARTCICLE_NOT_FOUND = "error.article.not.found";
-
 
 }
